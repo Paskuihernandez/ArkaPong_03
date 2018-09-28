022874c707f1f992ab7a3f64aa6bec3f46aa00e7
Ouva <victorvidal3d@gmail.com>
2018-09-28 10:28:51 +0200

    Commit_vic_28

--- PdA2_Android_BrickPong/Assets/Scripts/Ball_Controller.cs
+++ PdA2_Android_BrickPong/Assets/Scripts/Ball_Controller.cs
@@ -2,178 +2,134 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball_Controller : MonoBehaviour {
+public class Ball_Controller : MonoBehaviour
+{
 
-	//Untener una referencia para el rigidbody attachado al gameObject
-	Rigidbody rb;
-
-
-	// Use this for initialization
-	void Start () {
-
-		//Get shortcut to rigidbody component
-		rb = GetComponent<Rigidbody> ();
-
-		//Pause ball logic for 1.5 seconds, delay launch
-		StartCoroutine (Pause ());
+    //Tener una referencia para el rigidbody attachado al gameObject
+    public Rigidbody rb;
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
 
-		//Si la bola va a la izquierda
-		if (transform.position.x < -9.94f) {
+        //Get shortcut to rigidbody component
+        rb = GetComponent<Rigidbody>();
 
-			transform.position = Vector3.zero;
-			rb.velocity = Vector3.zero;
-
-			//El player 2 gana un punto
-			Scoreboard_Controller.instance.GivePlayerTwoAPoint();
+        //Pause ball logic for 1.5 seconds, delay launch
+        StartCoroutine(Pause());
 
 
-			StartCoroutine (Pause ());
-		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Si la bola va a la izquierda
+        if (transform.position.x < -9.94f)
+        {
+
+            transform.position = Vector2.zero;
+            rb.velocity = Vector2.zero;
+
+            //El player 2 gana un punto
+            Scoreboard_Controller.instance.GivePlayerTwoAPoint();
+
+
+            StartCoroutine(Pause());
+        }
 
 
         //Si la bola va a la derecha
-        if (transform.position.x > 9.94f) {
+        if (transform.position.x > 9.94f)
+        {
 
-			transform.position = Vector3.zero;
-			rb.velocity = Vector3.zero;
+            transform.position = Vector2.zero;
+            rb.velocity = Vector2.zero;
 
             //El player 1 gana un punto
             Scoreboard_Controller.instance.GivePlayerOneAPoint();
 
-			StartCoroutine (Pause ());
-		}
+            StartCoroutine(Pause());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("bottom"))
+        {
+            rb.velocity = Vector2.zero;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("brick"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 
 
-	}
-
-	IEnumerator Pause () {
-
-
-		//La bola vuelve salir en (1.5 seg):
-		yield return new WaitForSeconds (1.5f);
-
-		//Call function that launches the ball
-		LaunchBall ();
-	}
-
-	void LaunchBall () {
-
-		transform.position = Vector3.zero;
-
-		//Ball Chooses a direction
-		//Flies that direction
-
-		//Flip a coin, determine direction in x-axis
-		int xDirection = Random.Range(0, 2);
-
-		//Flip another coin, determine direction in y-axis
-		int yDirection = Random.Range(0, 3);
+    IEnumerator Pause()
+    {
 
 
-		Vector3 launchDirection = new Vector3 ();
+        //La bola vuelve salir en (1.5 seg):
+        yield return new WaitForSeconds(1.5f);
 
-		//Check results of one coin toss
-		if (xDirection == 0) {
+        //Call function that launches the ball
+        LaunchBall();
+    }
 
-			launchDirection.x = -8f;
-		}
-		if (xDirection == 1) {
+    void LaunchBall()
+    {
 
-			launchDirection.x = 8f;
-		}
+        transform.position = Vector2.zero;
 
-		//Check results of second coin toss
-		if (yDirection == 0) {
+        //Ball Chooses a direction
 
-			launchDirection.y = -8f;
-		}
-		if (yDirection == 1) {
+        //Flip a coin, determine direction in x-axis
+        int xDirection = Random.Range(0, 2);
 
-			launchDirection.y = 8f;
-		}
-		if (yDirection == 2) {
-
-			launchDirection.y = 0f;
-		}
-
-		//Assign velocity based off of where we launch ball
-		rb.velocity = launchDirection;
-	}
+        //Flip another coin, determine direction in y-axis
+        int yDirection = Random.Range(0, 3);
 
 
+        Vector2 launchDirection = new Vector2();
 
+        //Check results of one coin toss
+        if (xDirection == 0)
+        {
 
+            launchDirection.x = -8f;
+        }
+        if (xDirection == 1)
+        {
 
-	// When we hit something else...
-	void OnCollisionEnter (Collision hit) {
+            launchDirection.x = 8f;
+        }
 
-		//If it was the top or bottom of the screen...
-		if (hit.gameObject.name == "TopBounds") {
+        //Check results of second coin toss
+        if (yDirection == 0)
+        {
 
-			float speedInXDirection = 0f;
+            launchDirection.y = -8f;
+        }
+        if (yDirection == 1)
+        {
 
-			if (rb.velocity.x > 0f)
-				speedInXDirection = 8f;
+            launchDirection.y = 8f;
+        }
+        if (yDirection == 2)
+        {
 
-			if (rb.velocity.x < 0f)
-				speedInXDirection = -8f;
+            launchDirection.y = 0f;
+        }
 
-			rb.velocity = new Vector3 (speedInXDirection, -8f, 0f);
-		}
-
-		if (hit.gameObject.name == "BottomBounds") {
-			
-			float speedInXDirection = 0f;
-
-			if (rb.velocity.x > 0f)
-				speedInXDirection = 8f;
-
-			if (rb.velocity.x < 0f)
-				speedInXDirection = -8f;
-
-			rb.velocity = new Vector3 (speedInXDirection, 8f, 0f);
-		}
-
-		//If it was one of the bats
-		if (hit.gameObject.name == "Left_Bat") {
-
-			rb.velocity = new Vector3 (13f, 0f, 0f);
-
-
-			//Check if we hit lower half of the bat...
-			if (transform.position.y - hit.gameObject.transform.position.y < -1) {
-
-				rb.velocity = new Vector3 (8f, -8f, 0f);
-			}
-			//Check if we hit lower half of the bat...
-			if (transform.position.y - hit.gameObject.transform.position.y > 1) {
-
-				rb.velocity = new Vector3 (8f, 8f, 0f);
-			}
-		}
-		if (hit.gameObject.name == "Right_Bat") {
-
-			rb.velocity = new Vector3 (-13f, 0f, 0f);
-
-
-			//Check if we hit lower half of the bat...
-			if (transform.position.y - hit.gameObject.transform.position.y < -1) {
-
-				rb.velocity = new Vector3 (-8f, -8f, 0f);
-			}
-			//Check if we hit lower half of the bat...
-			if (transform.position.y - hit.gameObject.transform.position.y > 1) {
-
-				rb.velocity = new Vector3 (-8f, 8f, 0f);
-			}
-		}
-
-	}
-
+        //Assign velocity based off of where we launch ball
+        rb.velocity = launchDirection;
+    }
 }
