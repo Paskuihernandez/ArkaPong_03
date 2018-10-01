9c61dbfea94ca478ade8844bcd99a596cda3e8d1
JAVI PIXEL <JAVIFOTOH@GMAIL.COM>
2018-10-01 10:30:00 +0200

    BARRA se hace mas grande

---
+++ PdA2_Android_BrickPong/Assets/Scripts/IncrementandoseSize.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementandoseSize : MonoBehaviour
+{
+    
+    float y;
+    float x;
+    float z;
+
+    Vector3 nevSize;
+	
+	
+	void Update ()
+
+
+    {
+
+        z  = 0.10f;
+        x  = 0.5344744f;
+        y  += 1.568355f * Time.deltaTime;
+ 
+        transform.localScale = new Vector3(x, y, z);
+
+
+        if (transform.localScale.y >= 1.98399f)
+        {
+            y -= 1.568355f * Time.deltaTime;
+        }
+
+
+    }
+}
